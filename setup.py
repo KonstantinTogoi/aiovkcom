6f736b882931df79774fda1485ee86b34c2c3884
Konstantin Togoi <konstantin.togoi@gmail.com>
2020-04-10 14:50:41 +0300

    edit description

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Konstantin Togoi',
     author_email='konstantin.togoi@protonmail.com',
     url='https://github.com/KonstantinTogoi/aiovkcom',
-    description='vk.com Python REST API wrapper',
+    description='Python vk.com API wrapper',
     long_description=open('README.rst').read(),
     license='BSD',
     packages=find_packages(),
